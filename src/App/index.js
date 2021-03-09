7c04ce236379b6af16a7868d7638f769f4bbbe37
moisestech <m@moises.tech>
2021-03-09 06:46:15 -0500

    simplify App by imp func comp SketchWrapper

--- src/App/index.js
+++ src/App/index.js
@@ -1,33 +1,12 @@
 import React, { useRef } from "react";
-import Sketch from "react-p5";
-
-	let x = 50;
-	let y = 50;
+import SketchWrapper from '../components/Sketch'
 
 export default function App() {
-  const sketchContainer = useRef(null);
-
-	const setup = (p5, canvasParentRef) => {
-		// use parent to render the canvas in this ref
-		// (without that p5 will render the canvas outside of your component)
-		p5.createCanvas(500, 500).parent(canvasParentRef);
-    draw(p5);
-	};
-
-	const draw = (p5) => {
-		p5.background(0);
-		p5.ellipse(x, y, 70, 70);
-		// NOTE: Do not use setState in the draw function or in functions that are executed
-		// in the draw function...
-		// please use normal variables or class properties for these purposes
-		x++;
-	};
 
 	return (
     <div className="sketch-container">
       <h5>Linear-Regression with Tensorflow.js</h5>
-      <section ref={sketchContainer} className="section"></section>
-      {/* <Sketch setup={setup} draw={draw} /> */}
+      <SketchWrapper />
     </div>
   );
 };
