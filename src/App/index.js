7a51f3fd44da24ab4ae65b2364156cfee1fdeb32
moisestech <m@moises.tech>
2021-03-31 07:46:51 -0400

    prettier refactor

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import SketchWrapper from '../components/SketchWrapper'
+import React from 'react';
+import SketchWrapper from '../components/SketchWrapper';
 
 export default function App() {
-
-	return (
+  return (
     <div className="sketch-container">
       <h1>Linear-Regression with Tensorflow.js</h1>
       <SketchWrapper />
     </div>
   );
-};
+}
