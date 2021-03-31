7435d694fc87ae6128c77f0bd6c51f6191b34f52
moisestech <m@moises.tech>
2021-03-31 07:47:51 -0400

    prettier refactor

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 // linearly maps value from the range (a..b) to (c..d)
-export function mapRange (value, a, b, c, d) {
+export function mapRange(value, a, b, c, d) {
   // first map value from (a..b) to (0..1)
   value = (value - a) / (b - a);
   // then map it from (0..1) to (c..d) and return it
